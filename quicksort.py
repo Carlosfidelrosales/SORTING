0c29b31130a9e3699c2585b73cac07f0a273505a
Carlosfidelrosales <fidelcarlos302@gmail.com>
2023-01-21 13:15:51 +0800

    Added the last def function and inside it is the list of 10 numbers given and a print function that will display the sorted numbers.

--- quicksort.py
+++ quicksort.py
@@ -17,3 +17,11 @@ def quick_sort(numbers, low, high):
         partition_index = partition(numbers, low, high)
         quick_sort(numbers, low, partition_index - 1)
         quick_sort(numbers, partition_index + 1, high)
+
+
+def main():
+    numbers = [75, 43, 69, 73, 52, 86, 91, 49, 57, 1]
+    quick_sort(numbers, 0, len(numbers) - 1)
+    print(numbers)
+
+main()
